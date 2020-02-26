ec391c730c7cb48742891a436f13a6c71e2a1f13
kafrah <khalid.afrah@gmail.com>
2020-02-25 23:18:28 -0500

    Menu button, Javascript & Rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,9 @@
 // Select DOM Items
-const menuBtn = document.querySelector
-    ('.menu-btn');
-const menu = document.querySelector
-    ('.menu');
-const menuNav = document.querySelector
-    ('.menu-nav');
-const menuBranding = document.querySelector
-    ('.menu-branding');
-
-const navItems = document.querySelectorAll
-    ('.nav-item')
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initital State Of Menu
 let showMenu = false;
@@ -22,19 +16,18 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add
-            ('show'));
+        navItems.forEach(item => item.classList.add('show'));
 
         // Set Menu State
-        showMe = true;
+        showMenu = true;
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItem.forEach(item => item.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
 
         // Set Menu State
-        showMe = true;
+        showMenu = false;
     }
 }
